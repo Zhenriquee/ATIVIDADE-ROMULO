939ba7f29179856ddc88ea30103f88ae4e65b395
Zhenriquee <83546851+Zhenriquee@users.noreply.github.com>
2023-02-08 13:37:54 -0300

    ATIVIDADE 01 ATUALIZADA

--- AT-1.sql
+++ AT-1.sql
@@ -1,18 +1,34 @@
 -- LISTAR NOME DOS USUARIOS, INFORMAR SEXO,CASADOS, QUE RESIDEM EM CARUARU, APENAS OS ATIVOS E INFORMAR O NR DA CARTEIRA DO BENEFICIARIO.
 -- UTILIZAR TABELAS USUARIO, V_CTAS_MEDICAS, PRESTADOR.
 SELECT DISTINCT U.NM_SEGURADO,
-                U.TP_SEXO,
-                U.TP_ESTADO_CIVIL,
+                U.CD_MATRICULA,
+                ME.CD_CONTRATO,
                 U.SN_ATIVO,
+                U.TP_ESTADO_CIVIL,
                 C.DS_ENDERECO_COBRANCA,
                 C.DS_BAIRRO,
                 C.NM_CIDADE,
-                C.NM_UF,
-                ME.NR_CARTEIRA_BENEFICIARIO
+                C.NM_UF,      
+                CASE
+                  WHEN TO_CHAR(U.DT_CADASTRO) < '30/12/2020' THEN
+                  'CADASTRO  ANTIGO'
+                  ELSE
+                  'CADASTRO NOVO'
+               END AS INF_USUARIO,
+                CASE
+                  WHEN ME.VL_MENSALIDADE <= 189 THEN
+                   'PLANO 1'
+                  WHEN ME.VL_MENSALIDADE >= 190 OR ME.VL_MENSALIDADE <= 250 THEN
+                   'PLANO 2'
+                  WHEN ME.VL_MENSALIDADE >= 251 THEN
+                   'PLANO 3'
+                  ELSE
+                   'SEM CADASTRO'
+                END AS PLANO
   FROM DBAPS.USUARIO U
  INNER JOIN DBAPS.CONTRATO C
     ON U.CD_MATRICULA = C.CD_MATRICULA
- INNER JOIN DBAPS.V_CTAS_MEDICAS ME
+ INNER JOIN DBAPS.MENS_CONTRATO ME
     ON ME.CD_CONTRATO = C.CD_CONTRATO
  WHERE U.TP_ESTADO_CIVIL = 'C'
    AND C.NM_CIDADE = 'CARUARU'
